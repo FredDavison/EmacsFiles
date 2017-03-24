b7f2fc8eac9708ca53ce5bf354ae210d4c293bc9
FredDavison <fred.dev99@gmail.com>
2017-03-24 08:39:20 +0000

    Remove custom variable junk at end of file

--- init.el
+++ init.el
@@ -147,3 +147,20 @@
 	  (concat "C:/Users/fda/bin/GnuWin32/bin" ";" (getenv "PATH"))
 	  )
   )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("15348febfa2266c4def59a08ef2846f6032c0797f001d7b9148f30ace0d08bcf" default)))
+ '(package-selected-packages
+   (quote
+    (use-package nlinum-relative jedi helm fuzzy flycheck flatui-theme exec-path-from-shell evil-tabs evil-surround))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
