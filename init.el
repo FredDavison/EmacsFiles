c4a489da356b488c9dc4a6e1988b28b616af5938
FredDavison <fred.dev99@gmail.com>
2020-01-08 10:58:27 +0000

    Use helm-mini rather than helm-buffers-list

--- init.el
+++ init.el
@@ -179,6 +179,7 @@
   (setq helm-mode-fuzzy-match nil)
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  (global-set-key (kbd "C-x b") 'helm-mini)
   )
 
 (use-package smartparens
