228ed2900e34aaf9539a651d497acbeea9cceb80
FredDavison <fred.dev99@gmail.com>
2020-01-12 11:41:57 +0000

    Save rust buffer before test

--- rust.el
+++ rust.el
@@ -5,8 +5,15 @@
 ;;; Code:
 
 (define-key rust-mode-map (kbd "C-c C-c") 'rust-compile)
-(define-key rust-mode-map (kbd "C-c C-t") 'rust-test)
+(define-key rust-mode-map (kbd "C-c C-t") 'fcd-rust-save-test)
 
+
+(defun fcd-rust-save-test ()
+  (interactive)
+  (progn
+    (basic-save-buffer)
+    (rust-test))
+  )
 
 (provide 'rust)
 ;;; rust.el ends here
