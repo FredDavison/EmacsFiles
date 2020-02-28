a6e7050e861c58d1e9ce5f9d45030bf231595ee1
Fred Davison <fred.davison@dnvgl.com>
2020-02-28 12:18:50 +0000

    Make Helm prefer horizontal split

--- init.el
+++ init.el
@@ -189,6 +189,7 @@
   (require 'helm-config)
   (helm-mode t)
   (setq helm-mode-fuzzy-match nil)
+  (setq helm-window-prefer-horizontal-split t)
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
   (global-set-key (kbd "C-x b") 'helm-mini)
