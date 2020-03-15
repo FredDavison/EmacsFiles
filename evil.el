6f9e67b847e822d53531eec6e19f8906d47582e4
FredDavison <fred.dev99@gmail.com>
2020-03-15 09:23:48 +0000

    Define evil operator to substitute from 0 register

--- evil.el
+++ evil.el
@@ -1,6 +1,13 @@
+;;; Code:
 
 
-(evil-define-operator fcd/substitute-inside (delimiter)
+(evil-define-operator fcd/substitute-inside (beg end type register yank-handler)
   "Substitute text inside delimiter with text in 0 register."
   :type inclusive
-  )
+  (evil-delete beg end type)
+  (goto-char beg)
+  (let ((text (if register
+                  (evil-get-register register)
+                (evil-get-register 48))))
+    (insert text))
+  (goto-char beg))
