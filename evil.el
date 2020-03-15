2c0f3b0ebc43afefaaefe341c02f9359a3a218e1
FredDavison <fred.dev99@gmail.com>
2020-03-15 10:23:59 +0000

    Use prefix provided register if available

--- evil.el
+++ evil.el
@@ -1,13 +1,17 @@
 ;;; Code:
 
+(require 'evil)
 
 (evil-define-operator fcd/substitute-inside (beg end type register yank-handler)
-  "Substitute text inside delimiter with text in 0 register."
+  "Substitute text inside delimiter with text in 0 register or prefix-provided register."
   :type inclusive
-  (evil-delete beg end type)
+  (evil-delete beg end)
   (goto-char beg)
   (let ((text (if register
                   (evil-get-register register)
-                (evil-get-register 48))))
+                (evil-get-register (or evil-this-register 48)))))
     (insert text))
   (goto-char beg))
+
+(provide 'evil)
+;;; evil.el ends here
